051f293ca81fca70dc5d149ebc36c65b4a1c0085
Foxioo <foxioo2137@gmail.com>
2026-08-04 17:38:55 +0200

    "Hello World!" A basic window that draws two triangles using D3D8

---
+++ Square9to8.cpp
@@ -0,0 +1,184 @@
+
+/***********************************************************/
+//
+//  Copyright (c) 2024-2026 Foxioo 
+//
+/***********************************************************/
+
+#define STRICT
+#include <windows.h>
+#include <tchar.h>
+#include <D3DX8.h>
+#include "D3DApp.h"
+
+#pragma comment(lib, "legacy_stdio_definitions.lib")
+#pragma comment(lib, "d3d8.lib")
+#pragma comment(lib, "d3dx8.lib")
+#pragma comment(lib, "winmm.lib")
+
+/***********************************************************/
+
+#define D3DFVF_VERTEX_CUSTOM_STATIC (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+struct SETFVF_VERTEX_CUSTOM_STATIC
+{
+    FLOAT x, y, z, rhw;
+    DWORD color;
+    FLOAT u, v;
+};
+
+#define D3DFVF_VERTEX_CUSTOM (D3DFVF_XYZ | D3DFVF_DIFFUSE)
+struct SETFVF_VERTEX_CUSTOM {
+    FLOAT x, y, z;
+    DWORD color;
+};
+
+/***********************************************************/
+/*  
+    Colors!!11!!1! 
+    support function that convert the format like with shaders—where a value of 0.0 to 1.0 represents a color value of 0 to 255;
+*/
+D3DCOLOR cfloat4(float r, float g, float b, float a)
+{
+    int ri = (int)(r * 255.0f);
+    int gi = (int)(g * 255.0f);
+    int bi = (int)(b * 255.0f);
+    int ai = (int)(a * 255.0f);
+
+    return D3DCOLOR_RGBA(ri, gi, bi, ai);
+}
+
+D3DCOLOR cfloat3(float r, float g, float b)
+{
+    int ri = (int)(r * 255.0f);
+    int gi = (int)(g * 255.0f);
+    int bi = (int)(b * 255.0f);
+
+    return D3DCOLOR_XRGB(ri, gi, bi);
+}
+
+#define COLOR_BLACK     cfloat3(0.0f, 0.0f, 0.0f)
+#define COLOR_RED       cfloat3(1.0f, 0.0f, 0.0f)
+#define COLOR_GREEN     cfloat3(0.0f, 1.0f, 0.0f)
+#define COLOR_BLUE      cfloat3(0.0f, 0.0f, 1.0f)
+#define COLOR_WHITE     cfloat3(1.0f, 1.0f, 1.0f)
+
+/***********************************************************/
+/*
+    Scene Settings
+*/
+
+#define COLOR_BACKGROUND_RGB    cfloat3(0.0f, 0.0f, 0.0f)
+#define COLOR_BACKGROUND_A      1.0f
+
+#define D3D_ERASE_RENDER        1
+
+#define VERTEX_POINTS_COUNT     4
+#define VERTEX_TRANGLE_COUNT    2
+
+/***********************************************************/
+/*
+    Application class;
+    application class
+
+    bottom text :sob:
+*/
+class CMyD3DApplication : public CD3DApplication
+{
+    LPDIRECT3DVERTEXBUFFER8 D3D_VERTEX_BUFFOR;
+
+    protected:
+        // Initialize scene objects
+        HRESULT RestoreDeviceObjects() override
+        {
+            if (FAILED(m_pd3dDevice->CreateVertexBuffer(VERTEX_POINTS_COUNT * sizeof(SETFVF_VERTEX_CUSTOM_STATIC), 
+                                                        D3DUSAGE_WRITEONLY | D3DUSAGE_DYNAMIC,
+                                                        D3DFVF_VERTEX_CUSTOM_STATIC,
+                                                        D3DPOOL_DEFAULT,
+                                                        &D3D_VERTEX_BUFFOR)))
+            {
+                return E_FAIL;
+            }
+             
+                // Settings trangle
+
+                float posX = 100.0f;
+                float posY = 60.0f;
+                float posZ = 0.0f;
+
+                float sizeX = 200.0f;
+                float sizeY = 200.0f;
+            
+                SETFVF_VERTEX_CUSTOM_STATIC vertices[] =
+                {
+                    { posX,         posY,         posZ,     1.0f,   COLOR_RED,      0.0f,   0.0f },
+                    { posX + sizeX, posY,         posZ,     1.0f,   COLOR_GREEN,    1.0f,   0.0f },
+                    { posX,         posY + sizeY, posZ,     1.0f,   COLOR_BLUE,     0.0f,   1.0f },
+                    { posX + sizeX, posY + sizeY, posZ,     1.0f,   COLOR_WHITE,    1.0f,   1.0f }
+                };
+
+                VOID* vertices_p;
+
+                if (FAILED(D3D_VERTEX_BUFFOR->Lock(0, sizeof(vertices), (BYTE**)&vertices_p, 0)))
+                    return E_FAIL;
+
+            memcpy(vertices_p, vertices, sizeof(vertices));
+            D3D_VERTEX_BUFFOR->Unlock();
+
+            return S_OK;
+        }
+
+        // InvalidateDeviceObjects()
+        // read the name of the function; bruh
+        HRESULT InvalidateDeviceObjects() override
+        {
+            if (D3D_VERTEX_BUFFOR)
+            {
+                D3D_VERTEX_BUFFOR->Release();
+                D3D_VERTEX_BUFFOR = NULL;
+            }
+            return S_OK;
+        }
+
+        // Rendering scene
+        HRESULT Render() override
+        {
+            #if defined(D3D_ERASE_RENDER)
+                m_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, COLOR_BACKGROUND_RGB, COLOR_BACKGROUND_A, 0);
+            #endif
+
+            if (SUCCEEDED(m_pd3dDevice->BeginScene()))
+            {
+                // Drawing trangle
+                m_pd3dDevice->SetStreamSource(0, D3D_VERTEX_BUFFOR, sizeof(SETFVF_VERTEX_CUSTOM_STATIC));
+                m_pd3dDevice->SetVertexShader(D3DFVF_VERTEX_CUSTOM_STATIC);
+                m_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, VERTEX_TRANGLE_COUNT);
+
+                m_pd3dDevice->EndScene();
+            }
+            return S_OK;
+        }
+
+    public:
+        // Windows settings;
+        CMyD3DApplication()
+        {
+            m_strWindowTitle = (TCHAR*)_T("Hello World!");
+            m_bUseDepthBuffer = FALSE;
+            D3D_VERTEX_BUFFOR = NULL;
+        }
+};
+
+/***********************************************************/
+/*
+    Main function;
+    Calls the creation of a window from the CMyD3DApplication class;
+*/
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
+{
+    CMyD3DApplication d3d8app;
+
+    if (FAILED(d3d8app.Create(hInst)))
+        return 0;
+
+    return d3d8app.Run();
+}
